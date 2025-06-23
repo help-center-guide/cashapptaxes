b7e857de8ad26f578a359d052a9de98ef3c8594a
help-center-guide <nahidg2g@gmail.com>
2025-06-22 23:23:29 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -15,9 +15,9 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'setup.brother.com'
-copyright = '2025, setup.brother.com'
-author = 'setup.brother.com'
+project = 'Cash App Taxes'
+copyright = '2025, Cash App Taxes'
+author = 'Cash App Taxes'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
@@ -25,10 +25,10 @@ release = '1.0.0'
 # -- HTML output settings ----------------------------------------------------
 
 # Title shown in the browser tab and top of HTML pages
-html_title = "Setup.brother.com - Guide to Install a brother printer to my computer"
+html_title = "Cash App Taxes Setup Guide Create Account, Login"
 
 # Optional short title (e.g., for nav bar)
-html_short_title = "Guide to Install a brother printer to my computer"
+html_short_title = "Cash App Taxes Setup Guide Create Account, Login"
 
 # Favicon (place favicon.ico in the root or _static folder)
 html_favicon = 'favicon.ico'
